2a93268a20d796ce7509ddc7fabf276adace2e8d
Fajar Reiva Cahya <fajarreivacahya@gmail.com>
2024-08-03 22:04:14 +0700

    Update password-generator.py

--- password-generator.py
+++ password-generator.py
@@ -18,7 +18,7 @@ def main():
     password = generate_password(length)
     
     if password:
-        print(f"Generated password : {password}")
+        print(f"Password Yang Telah Di Buat : {password}")
 
 if __name__ == "__main__":
     main()
